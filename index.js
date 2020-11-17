3eabd92f1a3f25c993c0ae66d5a5743c9398a5d4
MADanson <matthewadjennings@gmail.com>
2020-11-17 17:22:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,44 @@
 const mqtt = require("mqtt");
 const axios = require("axios");
-const temp = require("pi-temperature")
+const temp = require("pi-temperature");
 
 //MQTT Connection
-const client = mqtt.connect("mqtt://192.168.1.104")
+const client = mqtt.connect("mqtt://192.168.1.104");
 
 client.on("connect", () => {
-    console.log("Connected to MQTT Broker")
-    client.subscribe("getTime")
-    client.subscribe("healthCheck/broker")
-})
+  console.log("Connected to MQTT Broker");
+  client.subscribe("getTime");
+  client.subscribe("healthCheck/broker");
+});
 
 client.on("error", (error) => {
-    console.log("An error occured: "+error)
-})
+  console.log("An error occured: " + error);
+});
 client.on("message", (topic, message) => {
-    switch(topic){
-        case("getTime"):
-            //console.log("Time Request!")
-            getTime().then((data)=> {
-                //console.log(data)
-                client.publish("timeSpaff", JSON.stringify(data))
-            })
-            break;
-        case("healthCheck/broker"):
-            var temp = temp.measure((err, temp) => {
-                if(err) console.error(err)
-                else return temp
-            })
-            client.publish("healthCheck/broker", `Power: ON, Temp: ${temp}`)
-            break;
-    }
-})
+  switch (topic) {
+    case "getTime":
+      //console.log("Time Request!")
+      getTime().then((data) => {
+        //console.log(data)
+        client.publish("timeSpaff", JSON.stringify(data));
+      });
+      break;
+    case "healthCheck/broker":
+      temp.measure(function (err, temp) {
+        if (err) console.error(err);
+        else client.publish("healthCheck/broker", `Power: ON, Temp: ${temp}`);
+      });
+      break;
+  }
+});
 //Api Call
-async function getTime(){  
-    const res = await axios.get("http://worldtimeapi.org/api/ip")
-    const data = {
-        time: res.data.datetime.substring(11,19),
-        day: res.data.day_of_week,
-        sunrise: "not available with this api",
-        sunset: "not available with this api"
-    }
-    return data
+async function getTime() {
+  const res = await axios.get("http://worldtimeapi.org/api/ip");
+  const data = {
+    time: res.data.datetime.substring(11, 19),
+    day: res.data.day_of_week,
+    sunrise: "not available with this api",
+    sunset: "not available with this api",
+  };
+  return data;
 }
-
