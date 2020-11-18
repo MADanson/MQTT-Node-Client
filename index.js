c1b2d889324527b3604cb0141814f0b598530eab
MADanson <matthewadjennings@gmail.com>
2020-11-18 20:15:10 +0000

    Added error handling for axios requests

--- index.js
+++ index.js
@@ -3,48 +3,72 @@ const axios = require("axios");
 const temp = require("pi-temperature");
 
 //MQTT Connection
-const client = mqtt.connect("mqtt://192.168.16.102");
+const client = mqtt.connect("mqtt://192.168.1.104");
+const timeApi = "http://worldtimeapi.org/api/ip"
+const weatherApi = "http://api.openweathermap.org/data/2.5/weather?q=Bristol,uk&units=metric&appid=b9c11ca10cec7e4ad57869a653171aee"
 
 client.on("connect", () => {
   console.log("Connected to MQTT Broker");
-  client.subscribe("getTime");
+  client.subscribe("get/time");
+  client.subscribe("get/sun");
   client.subscribe("healthCheck/broker");
 });
 
 client.on("error", (error) => {
   console.log("An error occured: " + error);
 });
-client.on("message", (topic, message) => {
+client.on("message", (topic) => {
   switch (topic) {
-    case "getTime":
-      //console.log("Time Request!")
+    case "get/time":
+      console.log("Time Request!")
       getTime().then((data) => {
-        //console.log(data)
-        client.publish("timeSpaff", JSON.stringify(data));
+        console.log(data)
+        client.publish("spaff/time", JSON.stringify(data));
+      }).catch((err) => {
+        console.log(err)
       });
       break;
-    case "healthCheck/broker":
-      var status = "Power: ON"
-    //   temp.measure(function (err, temp) {
-    //     if (err) {
-    //       console.log(error);
-    //     } else {
-    //       status = `Power: ON, Temp: ${temp}`;
-    //     }
-    //   });
-      console.log(status);
-      client.publish("healthCheck/broker", status);
-      break;
+    case "get/sun":
+        console.log("Sunrise Request!")
+        getSun().then((data) => {
+            console.log(data)
+            client.publish("spaff/sun", JSON.stringify(data));
+        }).catch((err) => {
+          console.log(err)
+        });
+        break;
   }
 });
 //Api Call
 async function getTime() {
-  const res = await axios.get("http://worldtimeapi.org/api/ip");
+  const res = await axios.get(timeApi);
+  //console.log(res.data);
   const data = {
     time: res.data.datetime.substring(11, 19),
     day: res.data.day_of_week,
-    sunrise: "not available with this api",
-    sunset: "not available with this api",
   };
   return data;
 }
+async function getSun() {
+    const res = await axios.get(weatherApi);
+    //console.log(res.data);
+    const data = {
+      sunrise: timeConvert(res.data.sys.sunrise),
+      sunset: timeConvert(res.data.sys.sunset)
+    };
+    return data;
+  }
+
+  function timeConvert(time){
+    const unix_time = time
+    
+    const date = new Date(unix_time * 1000);
+    const hours = date.getHours();
+    const minutes = "0" + date.getMinutes();
+    const seconds = "0" + date.getSeconds();
+    
+    const formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    
+    return formattedTime
+  }
+  
