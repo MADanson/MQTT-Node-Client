7af60a285afe544b2de085c59151a1be16d4e876
MADanson <matthewadjennings@gmail.com>
2020-11-18 23:15:56 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const temp = require("pi-temperature");
 
 //MQTT Connection
 const client = mqtt.connect("mqtt://192.168.1.104");
-const timeApi = "http://worldtimeapi.org/api/ip"
-const weatherApi = "http://api.openweathermap.org/data/2.5/weather?q=Bristol,uk&units=metric&appid=b9c11ca10cec7e4ad57869a653171aee"
+const timeApi = "http://worldtimeapi.org/api/ip";
+const weatherApi =
+  "http://api.openweathermap.org/data/2.5/weather?q=Bristol,uk&units=metric&appid=b9c11ca10cec7e4ad57869a653171aee";
 
 client.on("connect", () => {
   console.log("Connected to MQTT Broker");
@@ -20,23 +21,27 @@ client.on("error", (error) => {
 client.on("message", (topic) => {
   switch (topic) {
     case "get/time":
-      console.log("Time Request!")
-      getTime().then((data) => {
-        console.log(data)
-        client.publish("spaff/time", JSON.stringify(data));
-      }).catch((err) => {
-        console.log(err)
-      });
+      console.log("Time Request!");
+      getTime()
+        .then((data) => {
+          console.log(data);
+          client.publish("spaff/time", JSON.stringify(data));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
       break;
     case "get/sun":
-        console.log("Sunrise Request!")
-        getSun().then((data) => {
-            console.log(data)
-            client.publish("spaff/sun", JSON.stringify(data));
-        }).catch((err) => {
-          console.log(err)
+      console.log("Sunrise Request!");
+      getSun()
+        .then((data) => {
+          console.log(data);
+          client.publish("spaff/sun", JSON.stringify(data));
+        })
+        .catch((err) => {
+          console.log(err);
         });
-        break;
+      break;
   }
 });
 //Api Call
@@ -50,25 +55,25 @@ async function getTime() {
   return data;
 }
 async function getSun() {
-    const res = await axios.get(weatherApi);
-    //console.log(res.data);
-    const data = {
-      sunrise: timeConvert(res.data.sys.sunrise),
-      sunset: timeConvert(res.data.sys.sunset)
-    };
-    return data;
-  }
+  const res = await axios.get(weatherApi);
+  //console.log(res.data);
+  const data = {
+    sunrise: timeConvert(res.data.sys.sunrise),
+    sunset: timeConvert(res.data.sys.sunset),
+  };
+  return data;
+}
 
-  function timeConvert(time){
-    const unix_time = time
-    
-    const date = new Date(unix_time * 1000);
-    const hours = date.getHours();
-    const minutes = "0" + date.getMinutes();
-    const seconds = "0" + date.getSeconds();
-    
-    const formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-    
-    return formattedTime
-  }
-  
+function timeConvert(time) {
+  const unix_time = time;
+
+  const date = new Date(unix_time * 1000);
+  const hours = date.getHours();
+  const minutes = "0" + date.getMinutes();
+  const seconds = "0" + date.getSeconds();
+
+  const formattedTime =
+    hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
+
+  return formattedTime;
+}
