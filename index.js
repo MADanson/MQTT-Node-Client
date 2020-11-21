57e492920c218ef993ef588127d029c311a86058
MADanson <matthewadjennings@gmail.com>
2020-11-21 21:06:08 +0000

    Added SunData cache

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ require('dotenv').config()
 //const temp = require("pi-temperature");
 
 //MQTT Connection
-const client = mqtt.connect("mqtt://"+process.env.BROKER_IP);
+const client = mqtt.connect("mqtt://"+process.env.BROKER_IP, {username: "blinds", password: process.env.PASSKEY});
 const timeApi = "http://worldtimeapi.org/api/ip";
 const weatherApi =
   "http://api.openweathermap.org/data/2.5/weather?q="+process.env.CITY+",uk&units=metric&appid="+process.env.API_KEY;
+
+var cacheTimeout = Date.now()
+var sun = {
+  sunrise: 0,
+  sunset: 0,
+}
 
 client.on("connect", () => {
   console.log("Connected to MQTT Broker");
@@ -23,7 +29,7 @@ client.on("message", (topic) => {
   switch (topic) {
     case "get/time":
       console.log("Time Request!");
-      getTime()
+      getApiTime()
         .then((data) => {
           console.log(data);
           client.publish("spaff/time", JSON.stringify(data));
@@ -46,23 +52,31 @@ client.on("message", (topic) => {
   }
 });
 //Api Call
-async function getTime() {
+async function getApiTime() {
   const res = await axios.get(timeApi);
   console.log(res.data);
   const data = {
-    time: timeToDouble(res.data.datetime),
-    day: res.data.day_of_week,
+    time: Number(timeToDouble(res.data.datetime)),
+    day: res.data.day_of_week - 1,  
   };
   return data;
 }
+
 async function getSun() {
+  if(sun.sunrise !== 0 && (cacheTimeout) > Date.now()){
+    const time = new Date(cacheTimeout)
+    console.log("Will be fresh at: ", time.getHours(), ":", time.getMinutes(), ":", time.getSeconds())
+    console.log("Sending cached sun data")
+    return sun
+  }
   const res = await axios.get(weatherApi);
   //console.log(res.data);
-  const data = {
-    sunrise: timeConvert(res.data.sys.sunrise),
-    sunset: timeConvert(res.data.sys.sunset),
+  sun = {
+    sunrise: Number(timeConvert(res.data.sys.sunrise)),
+    sunset: Number(timeConvert(res.data.sys.sunset)),
   };
-  return data;
+  cacheTimeout = Date.now()  + 3600000
+  return sun;
 }
 
 function timeToDouble(time){
@@ -70,6 +84,7 @@ function timeToDouble(time){
     const timeArray = timeStr.split(":")
     return timeArray[0] + "." + timeArray[1]
 }
+
 function timeConvert(time) {
   const unix_time = time;
 
