e77d6dc7c4f3211636ed7de940fd6f6c5f3498a3
MADanson <matthewadjennings@gmail.com>
2020-11-24 16:12:45 +0000

    Added Healthchecker

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const mqtt = require("mqtt");
 const axios = require("axios");
+const ip = require("ip");
+const os = require("os");
 require("dotenv").config();
-//const temp = require("pi-temperature");
+const temp = require("pi-temperature");
 
 //MQTT Connection
 const client = mqtt.connect("mqtt://" + process.env.BROKER_IP, {
@@ -35,14 +37,7 @@ client.on("message", (topic) => {
   switch (topic) {
     case "get/time":
       console.log("Time Request!");
-      time()
-        .then((data) => {
-          // console.log(data);
-          
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      time();
       break;
     case "get/sun":
       console.log("Sunrise Request!");
@@ -54,6 +49,12 @@ client.on("message", (topic) => {
         .catch((err) => {
           console.log(err);
         });
+      break;
+    case "healthCheck/broker":
+      healthReport().then((data) => {
+        console.log(data);
+        client.publish("health/broker", JSON.stringify(data));
+      });
       break;
   }
 });
@@ -72,12 +73,12 @@ async function time() {
 
     console.log("Failed... Retrying");
     setTimeout(() => {
-      maxAttempts++
+      maxAttempts++;
     }, 2000);
   }
-  if(success === false) {
-    console.error("Time server timed out")
-    return null
+  if (success === false) {
+    console.error("Time server timed out");
+    return null;
   }
   // console.log(res);
   const data = {
@@ -91,9 +92,9 @@ async function time() {
   console.log(timeout, "Seconds till the minute");
 
   setTimeout(() => {
-    console.log("Spaffed Time")
+    console.log("Spaffed Time");
     client.publish("spaff/time", JSON.stringify(data));
-  }, (timeout*1000));
+  }, timeout * 1000);
 }
 
 async function timeApiCall() {
@@ -132,13 +133,31 @@ async function getSun() {
   return sun;
 }
 
+async function healthReport() {
+  let report = {
+    local_ip: ip.address(),
+    device: os.hostname(),
+    temp: getTemp(),
+    uptime: os.uptime(),
+  };
+  return report;
+}
+
+function getTemp(){
+  if(process.env.ISDEV) return "Script is in development mode"
+  temp.measure((err, temp) => {
+    if (err) console.error(err);
+    else return temp;
+  })
+}
+
 function timeToDouble(time) {
   const timeStr = time.substring(11, 19);
   const timeArray = timeStr.split(":");
-  if(timeArray[1] === 59){
+  if (timeArray[1] === 59) {
     timeArray[1] = 0;
-    if(timeArray[0] === 23){
-      timeArray[0] = 0
+    if (timeArray[0] === 23) {
+      timeArray[0] = 0;
     }
   }
   return timeArray[0] + "." + timeArray[1];
